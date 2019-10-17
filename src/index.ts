9adcadf117ecc1fe1ee3f96f1eac82acd7ebb89f
kiyon <kiyonlin@163.com>
2019-10-17 09:14:26 +0800

    myObservable v1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello TypeScript!');
+function myObservable(observer) {
+    for(let i = 0; i < 10; i++) {
+        observer.next(i);
+    }
+    observer.complete();
+}
+
+const observer = {
+    next: v => console.log(`next -> ${v}`),
+    error: ()=>{},
+    complete: () => console.log('complete'),
+};
+
+myObservable(observer);
